9d57945fdc75f4af4285632dcf78ca5944075755
Jason Wong <michaelgamedev@sina.cn>
2015-07-24 11:47:11 +0800

    add cc3vector operator overloading

--- cocos3d/Engine/libcocos3d/Common/CC3Vector.h
+++ cocos3d/Engine/libcocos3d/Common/CC3Vector.h
@@ -45,9 +45,9 @@ NS_COCOS3D_BEGIN
 class CC3Vector
 {
 public:
-	float			x;
-	float			y;
-	float			z;
+	float                   x;
+	float                   y;
+	float                   z;
 
 	/// Common constants
 	/** A CC3Vector of zero length at the origin. */
@@ -81,41 +81,72 @@ public:
 	static const CC3Vector	kCC3VectorUnitZNegative;
 
 public:
+    /* Default constructor */
 	inline CC3Vector() : x(0.f), y(0.f), z(0.f) {  }
+    
+    /* Constructor given three values*/
 	inline CC3Vector( float _x, float _y, float _z ) : x(_x), y(_y), z(_z) {  }
+    
+    /* Copy constructor */
+    inline CC3Vector( const CC3Vector& vector ) : x(vector.x), y(vector.y), z(vector.z) {  }
+    
+    /* Assignment operator, return reference to the lvlaue */
+    CC3Vector&              operator = ( const CC3Vector& other );
+    
+    /* Check for equality */
+    bool                    operator == ( const CC3Vector& other ) const;
+    bool                    operator != ( const CC3Vector& other ) const;
+    
+    /* Unary minus returns the negative of the vector */
+    CC3Vector               operator -() const;
+    
+    /* Binary + and - and subtract vectors */
+    CC3Vector               operator + ( const CC3Vector& other ) const;
+    CC3Vector               operator - ( const CC3Vector& other ) const;
+    
+    /* Multiplication and division by scalar */
+    CC3Vector               operator * ( float a ) const;
+    CC3Vector               operator / ( float a ) const;
+    
+    /* Combined assignment operators to conform to C notation convention */
+    CC3Vector&              operator += ( const CC3Vector &a );
+    CC3Vector&              operator -= ( const CC3Vector &a );
+    CC3Vector&              operator *= ( float a );
+    CC3Vector&              operator /= ( float a );
 
-	std::string		stringfy() const;
+    /* Convert CC3Vector to a string */
+	std::string             stringfy() const;
 
 	/** Returns whether the two vectors are equal by comparing their respective components. */
-	bool			equals( const CC3Vector& other ) const;
+	bool                    equals( const CC3Vector& other ) const;
 
 	/** Returns whether the specified vector is equal to the zero vector, specified by kCC3VectorZero. */
-	bool			isZero() const;
+	bool                    isZero() const;
 
 	/** Returns whether the specified vector is equal to the null vector, specified by kCC3VectorNull. */
-	bool			isNull() const;
+	bool                    isNull() const;
 
 	/**
 	 * Returns the result of scaling the original vector by the corresponding scale vector.
 	 * Scaling can be different for each axis. This has the effect of multiplying each component
 	 * of the vector by the corresponding component in the scale vector.
 	 */
-	CC3Vector		scale( const CC3Vector& scl );
+	CC3Vector               scale( const CC3Vector& scl );
 
 	/**
 	 * Returns the result of scaling the original vector by the corresponding scale
 	 * factor uniformly along all axes.
 	 */
-	CC3Vector		scaleUniform( float scl );
-	CC3Vector		scaleUniform( float scl ) const;
+	CC3Vector               scaleUniform( float scl );
+	CC3Vector               scaleUniform( float scl ) const;
 
 	/**
 	 * Returns a vector that is the negative of the specified vector in all directions.
 	 * For vectors that represent directions, the returned vector points in the direction
 	 * opposite to the original.
 	 */
-	CC3Vector		negate();
-	CC3Vector		negate() const;
+	CC3Vector               negate();
+	CC3Vector               negate() const;
 
 	/**
 	 * Returns a vector whose components comprise the minimum value of each of the respective
@@ -123,7 +154,7 @@ public:
 	 * one of the specified vectors, but a new vector, each of the components of which is the
 	 * minimum value for that component between the two vectors.
 	 */
-	CC3Vector		minimize( const CC3Vector& other );
+	CC3Vector               minimize( const CC3Vector& other );
 
 	/**
 	 * Returns a vector whose components comprise the maximum value of each of the respective
@@ -131,10 +162,10 @@ public:
 	 * one of the specified vectors, but a new vector, each of the components of which is the
 	 * maximum value for that component between the two vectors.
 	 */
-	CC3Vector		maxmize( const CC3Vector& other );
+	CC3Vector               maxmize( const CC3Vector& other );
 
 	/** Returns the dot-product of the two given vectors (v1 . v2). */
-	float			dot( const CC3Vector& other ) const;
+	float                   dot( const CC3Vector& other ) const;
 
 	/**
 	 * Returns the square of the scalar length of the specified CC3Vector from the origin.
@@ -143,20 +174,20 @@ public:
 	 * This function is useful for comparing vector sizes without having to run an
 	 * expensive square-root calculation.
 	 */
-	float			lengthSquared() const;
+	float                   lengthSquared() const;
 
 	/**
 	 * Returns the scalar length of the specified CC3Vector from the origin.
 	 * This is calculated as sqrt(x*x + y*y + z*z) and will always be positive.
 	 */
-	float			length() const;
+	float                   length() const;
 
 	/**
 	 * Returns a normalized copy of the specified CC3Vector so that its length is 1.0.
 	 * If the length is zero, the original vector (a zero vector) is returned.
 	 */
-	CC3Vector		normalize();
-	CC3Vector		normalize() const;
+	CC3Vector               normalize();
+	CC3Vector               normalize() const;
 
 	/**
 	 * Returns a CC3Vector that is the inverse of the specified vector in all directions,
@@ -166,26 +197,26 @@ public:
 	 * vector into 1.0 (1.0/x, 1.0/y, 1.0/z). It is the responsibility of the caller to
 	 * ensure that none of the components of the original is zero.
 	 */
-	CC3Vector		invert();
+	CC3Vector               invert();
 
 	/**
 	 * Returns the result of adding the two specified vectors, by adding the corresponding components 
 	 * of both vectors. This can also be thought of as a translation of the first vector by the second.
 	 */
-	CC3Vector		add( const CC3Vector& other );
-	CC3Vector		add( const CC3Vector& other ) const;
+	CC3Vector               add( const CC3Vector& other );
+	CC3Vector               add( const CC3Vector& other ) const;
 
 	/**
 	 * Returns the difference between two vectors, by subtracting the subtrahend from the minuend,
 	 * which is accomplished by subtracting each of the corresponding x,y,z components.
 	 */
-	CC3Vector		difference( const CC3Vector& other ) const;
+	CC3Vector               difference( const CC3Vector& other ) const;
 
 	/**
 	 * Returns a modulo version of the specifed rotation,
 	 * so that each component is between (+/-360 degrees).
 	 */
-	CC3Vector		rotationModulo();
+	CC3Vector               rotationModulo();
 
 	/**
 	 * Returns the difference between two rotation vectors, in terms of the minimal degrees,
@@ -198,10 +229,10 @@ public:
 	 * Similarly, the difference between 10 and 350 will yield +20 (ie- the smallest change from
 	 * 350 degrees to 10 degrees is +20 degrees) rather than -340 (from simple subtraction).
 	 */
-	CC3Vector		rotationalDifference( const CC3Vector& other );
+	CC3Vector               rotationalDifference( const CC3Vector& other );
 
 	/** Returns the positive scalar distance between the ends of the two specified vectors. */
-	float			distance( const CC3Vector& other ) const;
+	float                   distance( const CC3Vector& other ) const;
 
 	/**
 	 * Returns the square of the scalar distance between the ends of the two specified vectors.
@@ -209,7 +240,7 @@ public:
 	 * This function is useful for comparing vector distances without having to run an
 	 * expensive square-root calculation.
 	 */
-	float			distanceSquared( const CC3Vector& other ) const;
+	float                   distanceSquared( const CC3Vector& other ) const;
 
 	/**
 	 * Returns a vector that represents the average of the two specified vectors. This is
@@ -218,16 +249,16 @@ public:
 	 * The returned vector represents the midpoint between a line that joins the endpoints
 	 * of the two specified vectors.
 	 */
-	CC3Vector		average( const CC3Vector& other );
+	CC3Vector               average( const CC3Vector& other );
 
 	/** Returns the cross-product of the two given vectors (v1 x v2). */
-	CC3Vector		cross( const CC3Vector& other ) const;
+	CC3Vector               cross( const CC3Vector& other ) const;
 
 	/** Returns YES if the two vectors are either exactly parallel or exactly antiparallel. */
-	bool			isParallelWith( const CC3Vector& other ) const;
+	bool                    isParallelWith( const CC3Vector& other ) const;
 
 	/** Returns whether the two vectors are exactly perpendicular. */
-	bool			isPerpendicularTo( const CC3Vector& other ) const;
+	bool                    isPerpendicularTo( const CC3Vector& other ) const;
 
 	/**
 	 * Returns a linear interpolation between two vectors, based on the blendFactor.
@@ -235,11 +266,91 @@ public:
 	 * as v1 + (blendFactor * (v2 - v1)). If the blendFactor is either zero or one
 	 * exactly, this method short-circuits to simply return v1 or v2 respectively.
 	 */
-	CC3Vector		lerp( const CC3Vector& other, float blendFactor );
+	CC3Vector               lerp( const CC3Vector& other, float blendFactor );
 };
 
 //////////////////////////////////////////////////////////////////////////
 /// CC3Vector IMPLEMENTATION
+/* Assignment operator, return reference to the lvlaue */
+inline CC3Vector& CC3Vector::operator = ( const CC3Vector& other )
+{
+    x = other.x; y = other.y; z = other.z;
+    
+    return *this;
+}
+
+/* Check for equality */
+inline bool CC3Vector::operator == ( const CC3Vector& other ) const
+{
+    return x == other.x && y == other.y && z == other.z;
+}
+
+inline bool CC3Vector::operator != ( const CC3Vector& other ) const
+{
+    return x != other.x && y != other.y && z != other.z;
+}
+
+/* Unary minus returns the negative of the vector */
+inline CC3Vector CC3Vector::operator -() const
+{
+    return CC3Vector( -x, -y, -z );
+}
+
+/* Binary + and - and subtract vectors */
+inline CC3Vector CC3Vector::operator + ( const CC3Vector& other ) const
+{
+    return CC3Vector( x + other.x, y + other.y, z + other.z );
+}
+
+inline CC3Vector CC3Vector::operator - ( const CC3Vector& other ) const
+{
+    return CC3Vector( x - other.x, y - other.y, z - other.z );
+}
+
+/* Multiplication and division by scalar */
+inline CC3Vector CC3Vector::operator * ( float a ) const
+{
+    return CC3Vector( x * a, y * a, z * a );
+}
+
+inline CC3Vector CC3Vector::operator / ( float a ) const
+{
+    float oneOverA = 1.0f / a; // NOTE: no check for divide by zero here
+    
+    return CC3Vector( x * oneOverA, y * oneOverA, z * oneOverA );
+}
+
+// Combined assignment operators to conform to
+// C notation convention
+inline CC3Vector& CC3Vector::operator += ( const CC3Vector& other )
+{
+    x += other.x; y += other.y; z += other.z;
+    
+    return *this;
+}
+
+inline CC3Vector& CC3Vector::operator -= ( const CC3Vector& other )
+{
+    x -= other.x; y -= other.y; z -= other.z;
+    
+    return *this;
+}
+
+inline CC3Vector& CC3Vector::operator *= ( float a )
+{
+    x *= a; y *= a; z *= a;
+    
+    return *this;
+}
+
+inline CC3Vector& CC3Vector::operator /= ( float a )
+{
+    float	oneOverA = 1.0f / a;
+    x *= oneOverA; y *= oneOverA; z *= oneOverA;
+    
+    return *this;
+}
+
 inline std::string CC3Vector::stringfy() const
 {
 	return CC3String::stringWithFormat( (char*)"(%.3f, %.3f, %.3f)", this->x, this->y, this->z );
@@ -449,12 +560,18 @@ static GLfloat kCC3ScaleMin = 1.0e-9f;
  *
  * This is used to ensure that scales used in transforms do not cause uninvertable matrices.
  */
-static inline GLfloat CC3EnsureMinScaleAxis(GLfloat val) 
+static inline GLfloat CC3EnsureMinScaleAxis( GLfloat val )
 {
 	// Test in order of expected value, for fast return.
-	if (val > kCC3ScaleMin) return val;
-	if (val >= 0.0f) return kCC3ScaleMin;
-	if (val < -kCC3ScaleMin) return val;
+	if ( val > kCC3ScaleMin )
+        return val;
+    
+	if ( val >= 0.0f )
+        return kCC3ScaleMin;
+    
+	if ( val < -kCC3ScaleMin )
+        return val;
+    
 	return -kCC3ScaleMin;
 }
 
@@ -474,7 +591,7 @@ static inline CC3Vector CC3EnsureMinScaleVector( const CC3Vector& scale )
 }
 
 /** Returns a CC3Vector structure constructed from the vector components. */
-static inline CC3Vector CC3VectorMake(GLfloat x, GLfloat y, GLfloat z)
+static inline CC3Vector CC3VectorMake( GLfloat x, GLfloat y, GLfloat z )
 {
 	CC3Vector v;
 	v.x = x;
